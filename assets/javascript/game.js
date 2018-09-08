adc3f9a84b56900162d442115b509a061d54e812
Nicole W <Nicole.Wilkins2018@gmail.com>
2018-09-08 12:16:17 -0400

    individual word display

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,30 @@
 //number of wins
 
 //letters guessed
-var lettersGuessed = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var lettersGuessed = []
+//'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 //list of words
 var wordArray = ['old fashioned', 'margarita', 'moscow mule', 'whiskey sour', 'manhattan', 'mimosa', 'martini', 'daiquiri']
+
+
+//-select one randomly(random word generator)
+var wordGenerator = function() {
+    var wordIndex = Math.floor(Math.random() * wordArray.length);
+    return wordArray[wordIndex];
+}
+
+var randomWord = wordGenerator();
+    console.log("random person: " + randomWord);
+
+var tempString = "";
 
 //press any key to get started
 document.onkeyup = function(event) {
     var displayWords = document.getElementById("display-words");
     lettersGuessed.push(event.key.toLowerCase);
 
-    //userText.textcontent = event.key;
-    console.log("Number of words: " + wordArray.length)
-    var randomWord = wordGenerator();
-    console.log("random person: " + randomWord);
 
-    var tempString = "<p>";
+    
     var wins = 0
     var loses = 0
 
@@ -23,20 +32,12 @@ document.onkeyup = function(event) {
         if (lettersGuessed.includes(randomWord[i])) {
             tempString += randomWord[i] + " ";
         } else {
-        tempString += "_ _ ";
+        tempString += "_";
         }
     }
     //end paragraph
-    tempString += "<p>";
         console.log(tempString);
         displayWords.innerHTML = tempString;
 
 }
-//-select one randomly(random word generator)
-var wordGenerator = function() {
-    var wordIndex = Math.floor(Math.random() * wordArray.length);
-    return wordArray[wordIndex];
-}
 
-
-//press a key to begin again
